aee75f5d6e0ccfd2b67b1bd36c86ce05b1705e4c
Alex <alexjrussian@gmail.com>
2020-02-08 13:50:41 -0500

    changed recaptcha key in component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -82,7 +82,13 @@ const Contact = () => {
         Contact Me
       </PageHeader>
       <Content>
-        <Form action="/" name="contact" method="POST" data-netlify="true" data-netlify-recaptcha="true">
+        <Form
+          action="/"
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          data-netlify-recaptcha="true"
+        >
           <input type="hidden" name="form-name" value="contact" />
           <Label Htmlfor="name">Name</Label>
           <Input type="text" id="name" name="name" />
@@ -90,7 +96,10 @@ const Contact = () => {
           <Input type="email" id="email" name="email" />
           <Label Htmlfor="message">Message</Label>
           <TextArea id="message" name="message" />
-          <ReCAPTCHA onChange={recaptchaChangeHandler} />
+          <ReCAPTCHA
+            sitekey={process.env.REACT_APP_RECAPTCHA_KEY}
+            onChange={recaptchaChangeHandler}
+          />
           {!isHuman && (
             <Robot>Please confirm you are not a Robot and a Submit button will appear</Robot>
           )}
