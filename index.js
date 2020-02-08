7921e4755ee5eb2791fcaf4c99fbf2df843b6d88
Alex <alexjrussian@gmail.com>
2020-02-07 22:59:39 -0500

    removed code from index

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ app.listen(port, () => console.log(`Running on ${port}`));
 
 app.use(cors());
 app.use(express.json());
-app.use('/', router);
-app.listen(3002);
 
 const transport = {
   host: 'smtp.gmail.com',
